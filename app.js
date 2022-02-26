bb52ee1437fe0378942d9d1579479946247873ed
SuperSonic <supersonic@livemail.tw>
2022-02-26 07:31:24 +0000

    Update

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 require('dotenv').config()
 
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [
+	Intents.FLAGS.GUILDS,
+	Intents.FLAGS.GUILD_MESSAGES,
+	Intents.FLAGS.DIRECT_MESSAGES
+] });
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on('messageCreate', (message) => {
+	if (message.author.bot) return false; 
+	
+	console.log(`Message from ${message.author.username}: ${message.content}`);
+	message.author.send("Hi")
+})
 
 client.on('interactionCreate', async (interaction) => {
 	if (!interaction.isCommand()) return;
