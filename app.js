7d4aa6ca623259ef8fa6f63bf19ea2fa5ae32eca
SuperSonic <supersonic@livemail.tw>
2022-02-27 00:59:52 +0000

    Update

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 require('dotenv').config()
 
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [
-	Intents.FLAGS.GUILDS,
-	Intents.FLAGS.GUILD_MESSAGES,
-	Intents.FLAGS.DIRECT_MESSAGES
-] });
+const {
+	Client,
+	Intents
+} = require('discord.js');
+
+const client = new Client({
+	partials: [
+		"CHANNEL"
+	],
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.DIRECT_MESSAGES,
+		Intents.FLAGS.DIRECT_MESSAGE_TYPING
+	]
+});
 
 const triggers = {
 	MessageCreate: require('./message/index.js'),
--- message/index.js
+++ message/index.js
@@ -1,4 +1,18 @@
+const simple_commands = {
+	user_id: (message) => {
+		message.channel.send(message.author.tag)
+	},
+	clear: async (message) => {
+		const messages = await message.channel.messages.fetch()
+		messages.map((message) => message.delete())
+	}
+}
+
 module.exports = (message) => {
 	if (message.author.bot) return false;
 	console.log(`Message from ${message.author.username}: ${message.content}`);
+	if (message.content.startsWith('#')) {
+		const command_key = message.content.substring(1)
+		simple_commands[command_key](message)
+	}
 }
